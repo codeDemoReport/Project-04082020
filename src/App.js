48ad85ef457dfdbb0aa26dbdd67907cf1b961828
Tintt <truongthanhtin6@gmail.com>
2022-04-08 10:51:12 +0700

    [add] form login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <h1>This is App</h1>
+      <Home />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,46 @@
+import { Avatar, Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import React from "react";
+import { Form, Formik } from "formik";
+import CustomField from "../components/CustomField";
 
 function Home(props) {
   return (
-    <div>
-      <h1>This is Home</h1>
-    </div>
+    <section>
+      <Box>
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+      </Box>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={(values) => {}}
+      >
+        <Form>
+          <CustomField
+            name="email"
+            label="Email"
+            placeholder="Enter your email..."
+          />
+          <CustomField
+            name="password"
+            label="Password"
+            type="password"
+            placeholder="Enter your password..."
+          />
+          <Button type="submit" variant="contained">
+            Login
+          </Button>
+        </Form>
+      </Formik>
+    </section>
   );
 }
 
